4cc3c350b5538f7245c3b1d3557c9cc5b19b936a
onicuto <kiryauvachan@gmail.com>
2025-08-04 01:09:38 +0700

    Изменение мини-проекта Калькулятор

--- Lessons/Projects/Calculator.py
+++ Lessons/Projects/Calculator.py
@@ -1,7 +1,25 @@
-print("Введите первое число:")
-a = float(input())
-print("Введите второе число:")
-b = float(input())
+def ch(x):
+    try:
+        x = float(x)
+        return True
+    except ValueError:
+        return False
+
+
+a = input("Введите первое число:")
+if not ch(a):
+    print("Необходимо ввести число.")
+    exit(1)
+else:
+    a = float(a)
+
+b = input("Введите второе число:")
+if not ch(b):
+    print("Необходимо ввести число.")
+    exit(1)
+else:
+    b = float(b)
+
 print("""Выберите операцию 
        1. -
        2. +
@@ -10,12 +28,45 @@ print("""Выберите операцию
        5. **""")
 c = int(input())
 if c == 1:
-    print(a, " - ", b, " = ", a - b)
+    print(
+        f"{int(a) if a.is_integer() else a}",
+        " - ",
+        f"{int(b) if b.is_integer() else b}",
+        " = ",
+        a - b,
+    )
 elif c == 2:
-    print(a, " + ", b, " = ", a + b)
+    print(
+        f"{int(a) if a.is_integer() else a}",
+        " + ",
+        f"{int(b) if b.is_integer() else b}",
+        " = ",
+        a + b,
+    )
 elif c == 3:
-    print(a, " / ", b, " = ", a / b)
+    if b == 0:
+        print("На ноль делить нельзя")
+    else:
+        print(
+            f"{int(a) if a.is_integer() else a}",
+            " / ",
+            f"{int(b) if b.is_integer() else b}",
+            " = ",
+            a / b,
+        )
 elif c == 4:
-    print(a, " * ", b, " = ", a * b)
+    print(
+        f"{int(a) if a.is_integer() else a}",
+        " * ",
+        f"{int(b) if b.is_integer() else b}",
+        " = ",
+        a * b,
+    )
 elif c == 5:
-    print(a, " ** ", b, " = ", a**b)
+    print(
+        f"{int(a) if a.is_integer() else a}",
+        " ** ",
+        f"{int(b) if b.is_integer() else b}",
+        " = ",
+        a**b,
+    )
