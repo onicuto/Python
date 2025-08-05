e8b8e3fc1783e6fddecbf378a6c78214725f80c3
onicuto <kiryauvachan@gmail.com>
2025-08-05 17:37:32 +0700

    Изучение Dict

---
+++ Lessons/Dict.py
@@ -0,0 +1,5 @@
+country = {"code": "RU", "name": "Russia", "population": 144}
+# country = dict(code="RU", name="Russia", population=144)
+print(country["name"])
+for key, value in country.items():
+    print(key, ":", value)
